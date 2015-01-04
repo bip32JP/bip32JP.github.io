43ed182f3f8ea4e9f089668d71ed558aca347963
bip32jp <matsuringo.667@gmail.com>
2015-01-04 22:52:17 +0900

    Remove redundant js files

--- js/brainwallet_jp.js
+++ js/brainwallet_jp.js
@@ -1,7 +1,7 @@
 (function($){
 
-    var bip39 = new BIP39('en');
-    var isJP = false;
+    var bip39 = new BIP39('jp');
+    var isJP = true;
     var bip32_source_key = null;
     var bip32_derivation_path = null;
     var gen_from = "pass";
@@ -12,84 +12,84 @@
     var TIMEOUT = 600;
     var timeout = null;
 
-    var coin = "btc_main";
+    var coin = "mona_main";
 
     var COINS = {
         btc_main: {
-            name: "Bitcoin",
-            network: "Mainnet",
+            name: "ビットコイン",
+            network: "メインネット",
             prefix: 0,
             private_prefix: 0+0x80,
             bip32_public: BITCOIN_MAINNET_PUBLIC,
             bip32_private: BITCOIN_MAINNET_PRIVATE
         },
         btc_test: {
-            name: "Bitcoin",
-            network: "Testnet",
+            name: "ビットコイン",
+            network: "テストネット",
             prefix: 0x6f,
             private_prefix: 0x6f+0x80,
             bip32_public: BITCOIN_TESTNET_PUBLIC,
             bip32_private: BITCOIN_TESTNET_PRIVATE
         },
         doge_main: {
-            name: "Dogecoin",
-            network: "Mainnet",
+            name: "ドージュコイン",
+            network: "メインネット",
             prefix: 0x1e,
             private_prefix: 0x1e+0x80,
             bip32_public: DOGECOIN_MAINNET_PUBLIC,
             bip32_private: DOGECOIN_MAINNET_PRIVATE
         },
         doge_test: {
-            name: "Dogecoin",
-            network: "Testnet",
+            name: "ドージュコイン",
+            network: "テストネット",
             prefix: 0x71,
             private_prefix: 0x71+0x80,
             bip32_public: DOGECOIN_TESTNET_PUBLIC,
             bip32_private: DOGECOIN_TESTNET_PRIVATE
         },
         ltc_main: {
-            name: "Litecoin",
-            network: "Mainnet",
+            name: "ライトコイン",
+            network: "メインネット",
             prefix: 0x30,
             private_prefix: 0x30+0x80,
             bip32_public: LITECOIN_MAINNET_PUBLIC,
             bip32_private: LITECOIN_MAINNET_PRIVATE
         },
         ltc_test: {
-            name: "Litecoin",
-            network: "Testnet",
+            name: "ライトコイン",
+            network: "テストネット",
             prefix: 0x6f,
             private_prefix: 0x6f+0x80,
             bip32_public: LITECOIN_TESTNET_PUBLIC,
             bip32_private: LITECOIN_TESTNET_PRIVATE
         },
         mona_main: {
-            name: "Monacoin",
-            network: "Mainnet",
+            name: "モナーコイン",
+            network: "メインネット",
             prefix: 0x32,
             private_prefix: 0x32+0x80,
             bip32_public: MONACOIN_MAINNET_PUBLIC,
             bip32_private: MONACOIN_MAINNET_PRIVATE
         },
         mona_test: {
-            name: "Monacoin",
-            network: "Testnet",
+            name: "モナーコイン",
+            network: "テストネット",
             prefix: 0x6f,
             private_prefix: 0x6f+0x80,
             bip32_public: MONACOIN_TESTNET_PUBLIC,
             bip32_private: MONACOIN_TESTNET_PRIVATE
         },
         kuma_main: {
-            name: "Kumacoin",
-            network: "Mainnet",
+            name: "クマコイン",
+            network: "メインネット",
             prefix: 0x2d,
             private_prefix: 0x2d+0x80,
             bip32_public: KUMACOIN_MAINNET_PUBLIC,
             bip32_private: KUMACOIN_MAINNET_PRIVATE
         },
         kuma_test: {
-            name: "Kumacoin",
-            network: "Testnet",
+            name: "クマコイン",
+            network: "テストネット",
             prefix: 0x75,
             private_prefix: 0x75+0x80,
             bip32_public: KUMACOIN_TESTNET_PUBLIC,
@@ -97,35 +97,10 @@
         }
     };
 
-    entropy_array = ["00000000000000000000000000000000",
-                     "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
-                     "80808080808080808080808080808080",
-                     "ffffffffffffffffffffffffffffffff",
-                     "000000000000000000000000000000000000000000000000",
-                     "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
-                     "808080808080808080808080808080808080808080808080",
-                     "ffffffffffffffffffffffffffffffffffffffffffffffff",
-                     "0000000000000000000000000000000000000000000000000000000000000000",
-                     "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f",
-                     "8080808080808080808080808080808080808080808080808080808080808080",
-                     "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-                     "77c2b00716cec7213839159e404db50d",
-                     "b63a9c59a6e641f288ebc103017f1da9f8290b3da6bdef7b",
-                     "3e141609b97933b66a060dcddc71fad1d91677db872031e85f4c015c5e7e8982",
-                     "0460ef47585604c5660618db2e6a7e7f",
-                     "72f60ebac5dd8add8d2a25a797102c3ce21bc029c200076f",
-                     "2c85efc7f24ee4573d2b81a6ec66cee209b2dcbd09d8eddc51e0215b0b68e416",
-                     "eaebabb2383351fd31d703840b32e9e2",
-                     "7ac45cfe7722ee6c7ba84fbc2d5bd61b45cb2fe5eb65aa78",
-                     "4fa1a8bc3e6d80ee1316050e862c1812031493212b7ec3f3bb1b08f168cabeef",
-                     "18ab19a9f54a9274f03e5209a2ac8a91",
-                     "18a2e1d81b8ecfb2a333adcb0c17a5b9eb76cc5d05db91a4",
-                     "15da872c95a13dd738fbf50e427583ad61f18fd99f628c417a61cf8343c90419"]
-
-    var PUBLIC_KEY_VERSION = 0;
-    var PRIVATE_KEY_VERSION = 0x80;
+    var PUBLIC_KEY_VERSION = 0x32;
+    var PRIVATE_KEY_VERSION = 0x32+0x80;
     var ADDRESS_URL_PREFIX = ''
-    var BIP32_TYPE = BITCOIN_MAINNET_PRIVATE;
+    var BIP32_TYPE = MONACOIN_MAINNET_PRIVATE;
 
     function pad(str, len, ch) {
         padding = '';
@@ -186,9 +161,9 @@
         if( gen_from == 'pass' ) {
             $("#bip32_source_passphrase").attr('readonly', false);
             $("#bip39_passphrase").attr('readonly', false);
-            $("#bip32_source_key").attr('readonly', false);
+            $("#bip32_source_key").attr('readonly', true);
             $("#cancel_hash_worker").attr('disabled', false);
-            $("#gen_from_msg").html("Enter a Phrase to generate a BIP32 Wallet.");
+            $("#gen_from_msg").html("フレーズを記入してBIP32ウォレットを復元します。");
         } else {
             setErrorState($("#bip32_source_passphrase"), false);
             $("#bip32_source_passphrase").attr('readonly', true);
@@ -196,7 +171,7 @@
             $("#bip32_source_key").attr('readonly', false);
             stop_hash_worker();
             $("#cancel_hash_worker").attr('disabled', true);
-            $("#gen_from_msg").html("Enter a Master Private / Public Key to Generate a BIP32 Wallet.");
+            $("#gen_from_msg").html("マスタ秘密鍵及びマスタ公開鍵を入力してBIP32ウォレットを復元します。");
         }
     }
 
@@ -218,76 +193,10 @@
         }
     }
 
-    function createTestVectors() {
-        var seed
-        var salt
-        var hexseed
-        var xprv
-        
-        console.log( "[" );
-        console.log( "{" );
-        for (var i = 0; i < entropy_array.length; i++) {
-            seed = bip39.entropyToMnemonic(entropy_array[i])
-            salt = "㍍ガバヴァぱばぐゞちぢ十人十色"
-            console.log( "   \"entropy\": \"" + entropy_array[i] + "\"," );
-            console.log( "    \"phrase\": \"" + seed + "\"," );
-            console.log( "      \"salt\": \"" + salt + "\",");
-            seed = seed.normalize('NFKD');
-            salt = salt.normalize('NFKD');
-            hexseed = bip39.mnemonicToSeed(seed, salt);
-            console.log( "      \"seed\": \"" + hexseed + "\",");
-            
-            var hasher = new jsSHA(hexseed, 'HEX');   
-            var I = hasher.getHMAC("Bitcoin seed", "TEXT", "SHA-512", "HEX");
-            var il = Crypto.util.hexToBytes(I.slice(0, 64));
-            var ir = Crypto.util.hexToBytes(I.slice(64, 128));
-
-            var gen_bip32 = new BIP32();
-            try {
-                gen_bip32.eckey = new Bitcoin.ECKey(il);
-                gen_bip32.eckey.pub = gen_bip32.eckey.getPubPoint();
-                gen_bip32.eckey.setCompressed(true);
-                gen_bip32.eckey.pubKeyHash = Bitcoin.Util.sha256ripe160(gen_bip32.eckey.pub.getEncoded(true));
-                gen_bip32.has_private_key = true;
-
-                gen_bip32.chain_code = ir;
-                gen_bip32.child_index = 0;
-                gen_bip32.parent_fingerprint = Bitcoin.Util.hexToBytes("00000000");
-                gen_bip32.version = COINS[coin].bip32_private;
-                gen_bip32.depth = 0;
-
-                gen_bip32.build_extended_public_key();
-                gen_bip32.build_extended_private_key();
-            } catch (err) {
-                return;
-            }
-
-            xprv = gen_bip32.extended_private_key_string("base58")
-            console.log( "\"bip32_xprv\": \"" + xprv + "\"");
-            if (i != entropy_array.length - 1) {
-                console.log( "}," );
-                console.log( "" );
-                console.log( "{" );
-            } else {
-                console.log( "}" );
-                console.log( "]" );
-            }
-        }
-    };
-
     function onCancelHashWorkerClicked() {
         //stop_hash_worker();
 
-        if($("#ent_input").val()!=""){
-            try{
-                var seed = UseEntropy();
-            } catch(err) {
-                $("#bip32_source_passphrase").val("");
-                return;
-            }
-        } else {
-            var seed = bip39.generateMnemonic(($("input[name='wordcnt']:checked").val()/3)*32);
-        }
+        var seed = bip39.generateMnemonic();
         var seed_pw = $("#bip39_passphrase").val();
         seed_pw = seed_pw.normalize("NFKD");
         $("#bip32_source_passphrase").val(seed);
@@ -301,48 +210,6 @@
         //setWarningState($("#bip32_source_passphrase"), true, "The passphrase was hashed using a single SHA-256 and should be considered WEAK and INSECURE");
     }
 
-    function UseEntropy() {
-        var dice_rolls = $("#ent_input").val();
-        var base = $("input[name='base']:checked").val();
-        var words = $("input[name='wordcnt']:checked").val();
-        
-        if(base==6&&words==12&& !/^[0-5]{53}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 53 digits of 0-5");raise;};
-        if(base==6&&words==24&& !/^[0-5]{99}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 99 digits of 0-5");raise;};
-        if(base==20&&words==12&& !/^[A-Ja-j0-9]{31}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 31 digits of 0-9, A-J");raise;};
-        if(base==20&&words==24&& !/^[A-Ja-j0-9]{60}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 60 digits of 0-9, A-J");raise;};
-        if(base==16&&words==12&& !/^[A-Fa-f0-9]{32}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 32 digits of 0-9, A-F");raise;};
-        if(base==16&&words==24&& !/^[A-Fa-f0-9]{64}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 64 digits of 0-9, A-F");raise;};
-        if(base==2&&words==12&& !/^[0-1]{128}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 128 digits of 0-1");raise;};
-        if(base==2&&words==24&& !/^[0-1]{256}$/.test(dice_rolls) ){alert("Entropy incorrect\nPlease use 256 digits of 0-1");raise;};
-        
-        if(base!=16){
-            var bit128 = new BigInteger("100000000000000000000000000000000", 16);
-            var bit256 = new BigInteger("10000000000000000000000000000000000000000000000000000000000000000", 16);
-            
-            if(words==12){
-                var entropy = new BigInteger(dice_rolls, base).mod(bit128);
-            } else {
-                var entropy = new BigInteger(dice_rolls, base).mod(bit256);
-            }
-            
-            entropy = Crypto.util.bytesToHex(entropy.toByteArrayUnsigned());
-            
-        } else {
-            var entropy = dice_rolls
-        }
-        
-        var pad128 = "00000000000000000000000000000000"
-        var pad256 = "0000000000000000000000000000000000000000000000000000000000000000"
-        
-        if(words==12){
-            entropy = pad128.substring(0, pad128.length - entropy.length) + entropy
-        } else {
-            entropy = pad256.substring(0, pad256.length - entropy.length) + entropy
-        }
-        
-        return bip39.entropyToMnemonic(entropy);
-    }
-
     function updateSourcePassphrase() {
         var passphrase = $("#bip32_source_passphrase").val();
         var seed_pw = $("#bip39_passphrase").val();
@@ -352,7 +219,7 @@
         }
         if(!bip39.validate(passphrase)&&passphrase!=''){
         
-        alert( "Incorrect BIP39 Phrase" );
+        alert( "不正なフレーズです。" );
         
         } else if(passphrase!='') {
         
@@ -448,21 +315,21 @@
 
         if( isMasterKey(bip32_source_key) ) {
             if( bip32_source_key.has_private_key ) {
-                $("#bip32_key_info_title").html("<b>" + key_coin.name + " Master Private Key</b>");
+                $("#bip32_key_info_title").html("<b>" + key_coin.name + " マスタ秘密鍵</b>");
             } else {
-                $("#bip32_key_info_title").html("<b>" + key_coin.name + " Master Public Key</b>");
+                $("#bip32_key_info_title").html("<b>" + key_coin.name + " マスタ公開鍵</b>");
             }
         } else {
             if( bip32_source_key.has_private_key ) {
-                $("#bip32_key_info_title").html("<b>" + key_coin.name + " Derived Private Key</b>");
+                $("#bip32_key_info_title").html("<b>" + key_coin.name + " 派生秘密鍵</b>");
             } else {
-                $("#bip32_key_info_title").html("<b>" + key_coin.name + " Derived Public Key</b>");
+                $("#bip32_key_info_title").html("<b>" + key_coin.name + " 派生公開鍵</b>");
             }
         }
 
         var v = '' + pad8(bip32_source_key.version.toString(16));
-        if( bip32_source_key.has_private_key ) v = v + " (" + key_coin.name + " " + key_coin.network + " private key)";
-        else                                   v = v + " (" + key_coin.name + " " + key_coin.network + " public key)";
+        if( bip32_source_key.has_private_key ) v = v + " (" + key_coin.name + " " + key_coin.network + " 秘密鍵)";
+        else                                   v = v + " (" + key_coin.name + " " + key_coin.network + " 公開鍵)";
 
         $("#bip32_key_info_version").val(v);
 
@@ -576,8 +443,8 @@
             var checksum = Crypto.SHA256(Crypto.SHA256(bytes, {asBytes: true}), {asBytes: true}).slice(0, 4);
             $("#derived_private_key_wif").val(Bitcoin.Base58.encode(bytes.concat(checksum)))
         } else {
-            $("#derived_private_key").val("No private key available");
-            $("#derived_private_key_wif").val("No private key available");
+            $("#derived_private_key").val("秘密鍵は取得できません");
+            $("#derived_private_key_wif").val("秘密鍵は取得できません");
         }
 
         $("#derived_public_key").val(result.extended_public_key_string("base58"));
@@ -698,10 +565,6 @@
         }
     }
 
-    function update_length() {
-        $("#ent_cnt").html("Length: " + $("#ent_input").val().length);
-    }
-
     $(document).ready( function() {
 
         if (window.location.hash)
@@ -716,16 +579,14 @@
         $('#gen_from label input').on('change', onUpdateGenFrom );
         updateGenFrom();
 
-        $("#bip32_source_passphrase").val("awake book subject inch gentle blur grant damage process float month clown");
-        $("#bip32_source_key").val("xprv9s21ZrQH143K4KqQx9Zrf1eN8EaPQVFxM2Ast8mdHn7GKiDWzNEyNdduJhWXToy8MpkGcKjxeFWd8oBSvsz4PCYamxR7TX49pSpp3bmHVAY");
+        $("#bip32_source_passphrase").val("はんい　しねん　いこく　りりく　らしんばん　ととのえる　つうはん　せんぱい　たえる　はけん　じだい　うせつ");
+        $("#bip32_source_key").val("Mnpv3aDAjprQ2Rma8mKZdEML4MjkjSTeDYUTiM2DUX5g3cGrQbxT7MUXsGNtcjmU6mRbVB4N2ut4U6JEHhwM3wnjkDJETL95CkKWmr3eF6ZGjop");
         onInput("#bip32_source_passphrase", onUpdateSourcePassphrase);
         onInput("#bip39_passphrase", onUpdateSourcePassphrase);
-        
-        onInput("#ent_input", update_length);
 
         $("#checkbox_change_language").on('change', onLanguageChanged );
 
-        $("#cancel_hash_worker").on('click', onCancelHashWorkerClicked); // createTestVectors to generate vectors in console
+        $("#cancel_hash_worker").on('click', onCancelHashWorkerClicked);
         onInput("#bip32_source_key", onUpdateSourceKey);
         $("#bip32_hashing_progress_bar").width('100%');
         $("#cancel_hash_worker").attr('disabled', false);
